d54e31e90fb239e33c36517a4b7f5079c42a7abd
Maxime3399 <maxime3399.minecraft@gmail.com>
2018-11-27 16:15:41 +0100

    First Frame
    Création du core de la première fenêtre du projet.

--- src/fr/MaximeP/FractalOne/MainClass.java
+++ src/fr/MaximeP/FractalOne/MainClass.java
@@ -1,10 +1,20 @@
 package fr.MaximeP.FractalOne;
 
+import javax.swing.JFrame;
+
 public class MainClass {
 	
-	public void main(String[]args) {
+	static String version = "v1.0.0-a1.0";
+	
+	public static void main(String[]args) {
 		
+		JFrame frame = new JFrame();
 		
+		frame.setTitle("FractalOne "+version);
+		frame.setSize(1280, 720);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 		
 	}
 	
