ff71975bbea0182f48d7a4eb1709a1c0d02eb589
Maxime3399 <maxime3399.minecraft@gmail.com>
2018-12-11 16:27:28 +0100

    Clock
    Organisation du système de clock pour la génération de fractales.

--- src/fr/MaximeP/FractalOne/custom/Pan.java
+++ src/fr/MaximeP/FractalOne/custom/Pan.java
@@ -1,20 +1,18 @@
 package fr.MaximeP.FractalOne.custom;
 
 import java.awt.Graphics;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import javax.swing.JPanel;
  
 @SuppressWarnings("serial")
 public class Pan extends JPanel { 
 	
-	/*public Pan() {
-		
-		//none
-		
-	}*/
-	
 	public void paintComponent(Graphics g){
     
-		int a = 5;
+		/*int a = 5;
 		
 		for(int i = 0; i < 10; i++) {
 			
@@ -22,13 +20,34 @@ public class Pan extends JPanel {
 			a = a+100;
 			repaint();
 			System.out.println("[PAN] Exe "+i);
-			try {
-				Thread.sleep(1000);
-			}catch (Exception e) {
-				e.printStackTrace();
-			}
 			
-		}
+		}*/
+
+		final Runnable task = new Runnable() {
+			
+			int c = 0;
+			int a = 0;
+			
+			@Override
+			public void run() {
+				c++;
+				System.out.println("[PAN] Exe "+c);
+				
+				g.drawOval(1+a, 1+a, 100, 100);
+				a = a+100;
+				repaint();
+				
+				if(c >= 6) {
+					
+					
+					
+				}
+				
+			}
+		};
+		
+		final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+		executor.scheduleAtFixedRate(task, 0, 1, TimeUnit.SECONDS);
     
 	}
   
